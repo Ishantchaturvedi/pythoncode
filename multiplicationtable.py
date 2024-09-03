1f31bdab98c3ca56179c59e42f585afd16c19845
ishant kumar <110047012+Ishantchaturvedi@users.noreply.github.com>
2024-09-03 11:08:13 +0530

    Add files via upload

---
+++ multiplicationtable.py
@@ -0,0 +1,16 @@
+rows = int(input("Enter the number rows: "))
+
+for i in range(rows, 0, -1):
+    for space in range(1, rows -i + 1):
+        print(end= ' ')
+        
+    for star in range(1, i+1):
+        print('*', end=' ')
+    print('')
+for j in range(1, rows+1):
+    for space in range(1, rows - j + 1):
+        print(end=' ')
+        
+    for star in range(1, j+1):
+        print('*', end=' ')
+    print(' ')   
