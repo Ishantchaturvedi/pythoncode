a43a2425a6355fc12e59777c681bd08278c83ef1
Ishant kumar <110047012+Ishantchaturvedi@users.noreply.github.com>
2024-09-05 10:00:21 +0530

    Add files via upload

--- design.py
+++ design.py
@@ -1,28 +1,21 @@
-from turtle import*
-import colorsys
-bgcolor('black')
-tracer (500)
+import turtle
 
-def draw():
-    h = 0
-    for i in range(100):
-        c = colorsys.hsv_to_rgb(h,1,1)
-        h += 0.5
-        up()
-        goto(0,0)
-        down()
-        color('black')
-        fillcolor (c)
-        begin_fill()
-        rt (98)
-        circle(i, 12)
-        fd (290)
-        fd (i)
-        lt (29)
-        for j in range(129):
-            fd(i)
-            circle(j, 299, steps=2)
-        end_fill()
-draw()
-done()
-            
+t = turtle.Turtle()
+s = turtle.Screen()
+s.bgcolor("white")
+t.pencolor("red")
+a = 0
+b = 0
+t.speed(0)
+t.penup()
+t.goto(0,200)
+t.pendown()
+while True:
+    t.forward(a)
+    t.right(b)
+    a+=2
+    b+=1
+    if b == 210:
+        break
+
+    t.hideturtle()
